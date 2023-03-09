de0d4b55ba357c1a155241679f4584c324b20b2c
Rafael <devrafalcoelho@gmail.com>
2023-03-09 11:33:34 -0300

    review route

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,4 +1,5 @@
 const { knex } = require('../config/db')
+const { formatedValue, formatedDate } = require('../utils/format')
 
 const registerTransaction = async (req, res) => {
   const { type, value, date, description, categorie_id } = req.body
@@ -38,6 +39,44 @@ const registerTransaction = async (req, res) => {
   }
 }
 
+const detailTransaction = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const transaction = await knex('transactions')
+      .select(
+        'transactions.id',
+        'transactions.description',
+        'transactions.value',
+        'transactions.date',
+        'transactions.categorie_id',
+        'transactions.user_id',
+        'transactions.type'
+      )
+      .where('transactions.id', id)
+      .first()
+
+    if (!transaction) {
+      return res.status(404).json({ message: 'Transação não encontrada' })
+    }
+
+    const data = {
+      transactionId: transaction.id,
+      description: transaction.description,
+      value: formatedValue(transaction.value),
+      date: formatedDate(transaction.date),
+      categorie_id: transaction.categorie_id,
+      user_id: transaction.user_id,
+      type: transaction.type
+    }
+
+    return res.status(200).json(data)
+  } catch (error) {
+    return res.status(400).json({ message: error.message })
+  }
+}
+
 module.exports = {
-  registerTransaction
+  registerTransaction,
+  detailTransaction
 }
