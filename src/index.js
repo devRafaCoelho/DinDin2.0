cf7915d3962c7e01d5b5ffb9d8de77296b3ff5e4
Rafael <devrafalcoelho@gmail.com>
2023-03-14 13:49:55 -0300

    review

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const cors = require('cors')
 const { routes } = require('./routes')
 
+const PORT = process.env.PORT || 8000
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.API_PORT, () => {
-  console.log(`API connected on port ${process.env.API_PORT}`)
+app.listen(PORT, () => {
+  console.log(`API connected on port ${PORT}`)
 })
