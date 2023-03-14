4b7f0de602df47759996e25fa2911774ae6326fd
Rafael <devrafalcoelho@gmail.com>
2023-03-14 11:43:15 -0300

    review

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.DATABASE_URL,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
     //ssl: { rejectUnauthorized: false }
   }
 })
